f980a71d34889e3f535e95bb7ec85775d88db543
Legogo1R <superlego@inbox.ru>
2023-12-26 15:31:24 +0300

    Simplify code, devide into functions 1 blend file only, no Scene selection

--- main_functions.py
+++ main_functions.py
@@ -1,5 +1,13 @@
 import os, sys, subprocess, argparse, psutil
-import re
+import re, json
+
+import blendfile
+from config import (
+    SCRIPT_PATH,
+    PERM_SCRIPTS_PATH,
+    BLENDER_PATH,
+    MAIN_PATH
+)
 
 
 def createParser():
@@ -19,3 +27,70 @@ def is_blender_running():
     """
     
     return 'blender.exe'  in (p.name() for p in psutil.process_iter())
+
+def scripts2string(scrits_before, scripts_after,scripts_optional):
+    """
+    Converts scripts' pathes to single string needed as a command line argument
+    """
+
+    # Permanent scripts that should be run before and after optional scripts
+    tmp_lst = []
+    for script_name in scrits_before:
+        tmp_lst.append(os.path.sep.join([MAIN_PATH, PERM_SCRIPTS_PATH, script_name]))
+    arg_scripts_before = " ".join(tmp_lst)  # Needs to be as 1 string
+    
+    tmp_lst = []
+    for script_name in scripts_after:
+        tmp_lst.append(os.path.sep.join([MAIN_PATH, PERM_SCRIPTS_PATH, script_name]))
+    arg_scripts_after = " ".join(tmp_lst)  # Needs to be as 1 string
+
+    # Optional scripts that can be passed as arguments, will be run in between
+    tmp_lst = []
+    for script_name in scripts_optional:
+        tmp_lst.append(os.path.sep.join([MAIN_PATH, SCRIPT_PATH, script_name]))
+    arg_optional_scripts = " ".join(tmp_lst)  # Needs to be as 1 string
+
+    return f'-P {arg_scripts_before} {arg_optional_scripts} {arg_scripts_after}'
+
+# Unbeliveable shit below.. who could have guessed that you can do that..
+def get_scene_names(blend_file):
+    """
+    Get scene names from a blend file without even opening it ! WHAAAT?!
+    """
+
+    def get_id_name(block):
+        name = block[b'id', b'name'].decode()
+        return name[2:]
+
+    bf = blendfile.open_blend(blend_file)
+    scene_block = bf.find_blocks_from_code(b'SC')
+    scenes = [get_id_name(sc) for sc in scene_block]
+    print(scenes)
+    bf.close()
+
+    return scenes
+
+def create_arg_scenes(scenes, render_frames):
+
+    tmp_scenes = []
+    if render_frames['render'] == 'f':
+        for scene in scenes:
+            tmp_scenes.append(f'-S {scene} -f {render_frames["frames"]}')
+    if render_frames['render'] == 'a':
+        for scene in scenes:
+            tmp_scenes.append(f'-S {scene} -a')
+    
+    arg_scenes = " ".join(tmp_scenes)  # Needs to be as 1 string
+    return arg_scenes
+
+
+def render(arg_blend_file, arg_scripts, arg_scenes):
+    """ 
+    Runs blender through command line to render with desired settings
+    """
+
+    blender = BLENDER_PATH
+    render = subprocess.run(
+            f'{blender} {arg_blend_file} {arg_scripts} {arg_scenes}',
+            shell = True
+        )
--- render_launch.py
+++ render_launch.py
@@ -1,15 +1,24 @@
 import os, sys, subprocess, argparse, psutil
-import re
+import re, json
 
 from config import *
-from main_functions import createParser, is_blender_running
+from main_functions import (
+    createParser,
+    is_blender_running,
+    scripts2string,
+    get_scene_names,
+    create_arg_scenes,
+    render
+)
 
 
-scripts_path = 'f:\\Library\\Blender_Work\\3Devision\\Scripts\\Render_cmd\\Scripts\\'
-scene_name_generator = f'-P {scripts_path}scene_name_generator_for_cmd.py'
-rendersettings = f'-P {scripts_path}rendersettings.py'
-viewlayers_use_for_rendering = f'-P {scripts_path}viewlayers_use_for_rendering_from_every_scene.py'
-active_layers_printer = f'-P {scripts_path}active_layers_printer.py'
+# Inforamtion about wich frames to render
+# Taken from rendersettings.json for eqch .blend file
+path = os.path.sep.join([MAIN_PATH, 'rendersettings.json'])
+with open(path, 'r') as fp:
+    config = json.load(fp)
+
+render_frames = config['render_frames']
 
 
 
@@ -27,97 +36,16 @@ if __name__ == '__main__':
     parser = createParser()
     args = parser.parse_args(sys.argv[1:])
 
-    blender = BLENDER_PATH
+    # Arguments for rendering througch command line
+    blend_file = args.blend[0]
+    arg_blend_file = f'-b {blend_file}'
+    arg_scripts = scripts2string(SCRIPTS_BEFORE,SCRIPTS_AFTER,args.script)
 
-    # Permanent scripts that should be run before and after optional scripts
-    tmp_lst = []
-    for script_name in SCRIPTS_BEFORE:
-        tmp_lst.append(os.path.sep.join([os.path.abspath(PERM_SCRIPTS_PATH), script_name]))
-    arg_scripts_before = " ".join(tmp_lst)  # Needs to be as 1 string
-    
-    tmp_lst = []
-    for script_name in SCRIPTS_AFTER:
-        tmp_lst.append(os.path.sep.join([os.path.abspath(PERM_SCRIPTS_PATH), script_name]))
-    arg_scripts_after = " ".join(tmp_lst)  # Needs to be as 1 string
-
-    # Optional scripts that can be passed as arguments, will be run in between
-    tmp_lst = []
-    for script_name in args.script:
-        tmp_lst.append(os.path.sep.join([os.path.abspath(SCRIPT_PATH), script_name]))
-    arg_optional_scripts = " ".join(tmp_lst)  # Needs to be as 1 string
-
-    print(arg_scripts_before)
-    print(arg_optional_scripts)
-    print(arg_scripts_after)
-
-
-    scene_names_string = ''
-    
 
     print('\n..Getting scene names from .blend files..')
-#     #Getting scene names cycle
-#     scene_names_dict = {}
-#     for file_number, bl in enumerate(args.blend):
-#         blend = f'-b {bl}'
-#         #First launch of blender to get scene names
-#         scene_names = subprocess.run(
-#             f'{blender} {blend} {scene_name_generator}',
-#             shell = True,
-#             capture_output=True
-#         )
-#         scene_names_dict[file_number] = scene_names.stderr.strip().decode("utf-8")
-#         print(f'\n..{bl} Done! [{file_number+1}/{len(args.blend)}]..')
+    scenes = get_scene_names(blend_file)
+    arg_scenes = create_arg_scenes(scenes, render_frames)
 
-#     if args.L == True:
-#         #Working with scene names to print(scene to index : scene column), input(index), print(selected scenes)
-#         for i in range(len(args.blend)):
-#             print(f'\n..Type in Indexes of Scenes you want to render for {args.blend[i]}, enumerated through \'space\'..\n\
-# - Hit "Enter" to render no scenes\n\
-# - Type in "a" to render all scenes\n')
-#             splited_list = re.split(r"(\s+-a\s+|\s+-a|\s-f\s+\d\s+|\s-f\s+\d|\s-f\s+\d..\d\s+|\s-f\s+\d..\d|\s-f\s+\d,\d\s+|\s-f\s+\d,\d)", scene_names_dict[i])
-#             del splited_list[-1]
-#             scene_names_list = []
-#             scenes = [] #For printing only selected '-S Scene'
-#             number_of_scenes = []
-#             for index in range(len(splited_list)//2):
-#                 number_of_scenes.append(str(index))
-#                 scene_names_list.append(f"{splited_list[2*index]}{splited_list[2*index + 1]}")
-#                 temp_str = f"{splited_list[2*index]}"
-#                 scenes.append(f"{splited_list[2*index]} ")
-#                 print(f'{index} : {temp_str}')
-#             # Waits for input of scene's indexes and then append them to script string
-#             scenes_index_list = input()
-#             temp_scene_names_list = []
-#             temp_scenes = '' #For printing only selected '-S Scene'
-#             while all(
-#                 scenes_index_list == 'a'
-#                 or scenes_index_list == ''
-#                 or x in number_of_scenes for x in scenes_index_list.split(' ')
-#                 ) == False:
-#                 scenes_index_list = input('Wrong input\n')
-#             else:
-#                 if scenes_index_list == 'a':
-#                     temp_scenes = 'All Scenes to render'
-#                 elif scenes_index_list == '':
-#                     scene_names_dict[i] = ""
-#                     temp_scenes = '0 Scenes to render'
-#                 else:
-#                     scenes_index_list = scenes_index_list.split(' ')
-#                     for index in scenes_index_list:
-#                         temp_scene_names_list.append(scene_names_list[int(index)])
-#                         scene_names_dict[i] = " ".join(temp_scene_names_list)
-#                         temp_scenes += scenes[int(index)]
-#             print(f'..You have selected {temp_scenes}..')
-#     else:
-#         pass
-#     # Rendering cycle
-#     for file_number, bl in enumerate(args.blend):
-#         blend = f'-b {bl}'
-#         scenes_to_render = scene_names_dict[file_number]
-#         print(f'###################\n{blender} {blend} {scripts_before} {script} {scripts_after} {scenes_to_render}\n###################')
-
-#         # Second launch of blender to render
-#         render = subprocess.run(
-#             f'{blender} {blend} {scripts_before} {script} {scripts_after} {scenes_to_render}',
-#             shell = True
-#         )
+    
+    # Rendering cycle
+    render(arg_blend_file, arg_scripts, arg_scenes)

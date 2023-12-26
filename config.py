5b04355392a8c8c5317d42ee4043a1ac6023ce78
Legogo1R <superlego@inbox.ru>
2023-12-26 15:35:12 +0300

    removed old scripts

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 import os
 
+MAIN_PATH = 'f:\\Library\\Blender_Work\\3Devision\\Scripts\\Render_cmd'
 SCRIPT_PATH = "Scripts"
 PERM_SCRIPTS_PATH = os.path.sep.join([SCRIPT_PATH, "Permanent"])
-BLENDER_PATH = 'c:\App\Blender\stable\blender-4.0.0+stable.878f71061b8e\blender.exe'
-
+BLENDER_PATH = 'c:\\App\\Blender\stable\\blender-4.0.0+stable.878f71061b8e\\blender.exe'
+TEMP_PATH = ".temp"
 
 
 SCRIPTS_BEFORE = [
     'rendersettings.py',
-    'viewlayers_use_for_rendering'
 ]
 
 SCRIPTS_AFTER = [
-    'active_layers_printer.py',
 
 ]
 

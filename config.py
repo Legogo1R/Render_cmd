17ec08fcb639c0341c226b4b007e5ba2bcfbb92c
Legogo1R <superlego@inbox.ru>
2023-12-27 23:25:03 +0300

    tweaks

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
-MAIN_PATH = 'f:\\Library\\Blender_Work\\3Devision\\Scripts\\Render_cmd'
-SCRIPT_PATH = "Scripts"
-PERM_SCRIPTS_PATH = os.path.sep.join([SCRIPT_PATH, "Permanent"])
 BLENDER_PATH = 'c:\\App\\Blender\stable\\blender-4.0.0+stable.878f71061b8e\\blender.exe'
-TEMP_PATH = ".temp"
+
+MAIN_PATH = 'f:\\Library\\Blender_Work\\3Devision\\Scripts\\Render_cmd'
+SCRIPT_PATH = 'blender_scripts'
+PERM_SCRIPTS_PATH = os.path.sep.join([SCRIPT_PATH, 'Permanent'])
+MISC_PATH = 'misc'
 
 
 SCRIPTS_BEFORE = [

c343d3f2476e769e7baf7a81ae06157c09b18e45
Legogo1R <superlego@inbox.ru>
2024-01-07 13:54:48 +0300

    Simplified path

--- config.py
+++ config.py
@@ -2,16 +2,22 @@ import os
 
 BLENDER_PATH = 'c:\\App\\Blender\stable\\blender-4.0.0+stable.878f71061b8e\\blender.exe'
 
+###
+# Do not forget to change path in 'rendersettings.py'
+###
+
 MAIN_PATH = 'f:\\Library\\Blender_Work\\3Devision\\Scripts\\Render_cmd'
 SCRIPT_PATH = 'blender_scripts'
-PERM_SCRIPTS_PATH = os.path.sep.join([SCRIPT_PATH, 'Permanent'])
+PERM_SCRIPTS_PATH = 'Permanent'
 MISC_PATH = 'misc'
+RSYNC_FOLDER_PATH = 'd:\\Library\\RSync\\'
 
-
+# Scripts to run before Optional(Extra) sripts
 SCRIPTS_BEFORE = [
     'rendersettings.py',
 ]
 
+# Scripts to run after Optional(Extra) sripts
 SCRIPTS_AFTER = [
 
 ]

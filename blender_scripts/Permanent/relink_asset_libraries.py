25b2ed8aff79b72615c0bd3565040508b45bf397
Legogo1R <superlego@inbox.ru>
2024-01-10 14:44:42 +0300

    test

--- blender_scripts/Permanent/relink_asset_libraries.py
+++ blender_scripts/Permanent/relink_asset_libraries.py
@@ -1,5 +1,11 @@
 import bpy
+import os, sys
 
-os.environ['Render_cmd']
+#os.environ['Render_cmd'] = r'f:\Library\Blender_Work\3Devision\Scripts\Render_cmd'
 
-bpy.ops.file.find_missing_files()
+#sys.path.insert(1, os.environ['Render_cmd'])
+
+#from config import ASSET_LIBRARY_PATH
+#print(ASSET_LIBRARY_PATH)
+
+bpy.ops.file.find_missing_files(directory=r'f:\Library\Blender_Work\3Devision\T124_SHORT_SOHO_SUITES')

58eea40b131767bbeacac7fb4f51044cb2e145ad
Legogo1R <superlego@inbox.ru>
2024-01-10 16:54:40 +0300

    small fixes

--- config_example.py
+++ config_example.py
@@ -6,9 +6,11 @@ MISC_PATH = 'misc'
 RSYNC_FOLDER_PATH = r''
 ASSET_LIBRARY_PATH = r''
 
+# Scripts will be executed from top - down
 # Scripts to run before Optional(Extra) sripts
 SCRIPTS_BEFORE = [
-    'rendersettings.py',
+    'relink_asset_libraries.py',  # Permanent (Don't remove)
+    'rendersettings.py',  # Permanent (Don't remove)
 ]
 
 # Scripts to run after Optional(Extra) sripts

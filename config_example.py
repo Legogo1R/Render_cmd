ee74ff1cf9ee319d193bf12f8d15b9b2a8dd2738
Legogo1R <superlego@inbox.ru>
2024-01-10 14:50:24 +0300

    init

---
+++ config_example.py
@@ -0,0 +1,17 @@
+BLENDER_PATH = r'path/to/blender.exe'
+MAIN_PATH = r'path/to/repository'
+SCRIPT_PATH = 'blender_scripts'
+PERM_SCRIPTS_PATH = 'Permanent'
+MISC_PATH = 'misc'
+RSYNC_FOLDER_PATH = r''
+ASSET_LIBRARY_PATH = r''
+
+# Scripts to run before Optional(Extra) sripts
+SCRIPTS_BEFORE = [
+    'rendersettings.py',
+]
+
+# Scripts to run after Optional(Extra) sripts
+SCRIPTS_AFTER = [
+
+]
